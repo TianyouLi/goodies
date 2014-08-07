500cdf3650a7d2c9e505bd4cccc51b1469e816b8
Tianyou Li <tianyou.li@gmail.com>
2014-08-07 11:28:24 +0800

    dev image

---
+++ packer/scripts/launch-ubuntu-dev.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#######################################################################
+## The command line to start a container for costa testing
+#  The parameters explained as below:
+#
+#  1. current stable image: tli7-sto.sh.intel.com:5000/costa-dev:0.6
+#  2. port mapping:
+#     ssh:                host 8022  -> container 22
+#     vnc:                host 50389 -> container 3389
+#     telemetry rest api: host 50808 -> container 8080
+#######################################################################
+## Above information will update according to any changes of system!!!
+#######################################################################
+
+sudo docker run --privileged -d -h lity-ubuntu-dev --name "lity-ubuntu-dev" -p 8022:22 -p 50389:3389  lity/ubuntu-dev:0.1 /bin/bash -e /etc/init.d/run.sh
--- packer/ubuntu-dev/deploy.sh
+++ packer/ubuntu-dev/deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+function runcmd() {
+		echo $1
+		eval $1
+		echo "$1 ... done!"
+}
+
 # now setup some environment, first let's change privoxy settings
 echo "forward-socks5 / proxy.jf.intel.com:1080 ." >> /etc/privoxy/config
 echo "listen-address 127.0.0.1:8118" >> /etc/privoxy/config
@@ -18,18 +24,53 @@ Acquire::https::proxy \"http://proxy01.cd.intel.com:911\";
 Acquire::ftp::proxy \"http://proxy01.cd.intel.com:911\";
 Acquire::socks::proxy \"socks://proxy.jf.intel.com:1080/\";" >> /etc/apt/apt.conf
 
-# then set link
-echo "#!/bin/sh
+# chmod for run.sh
+chmod +x /etc/init.d/run.sh
 
-echo $1 | grep \"\.intel\.com$\" > /dev/null 2>&1
+# add user
+adduser --disabled-password --gecos ""  lity
+
+# change user passwd
+echo 'lity:123456' | chpasswd
+
+
+# modify sudoers file
+echo "Defaults        env_keep = \"http_proxy ftp_proxy all_proxy https_proxy no_proxy socks_proxy\"
+# Allow krom to have root access for ChromeOS build
+lity ALL=(ALL) NOPASSWD: ALL" | tee -a /etc/sudoers
+
+
+## append environment vars
+echo 'export http_proxy="http://localhost:8118"
+export socks_proxy="proxy.jf.intel.com:1080"
+
+export all_proxy=${socks_proxy}
+export ftp_proxy=${http_proxy}
+export https_proxy=${http_proxy}
+export RSYNC_PROXY=${http_proxy}
+
+export no_proxy=".intel.com,.jf.intel.com,.sh.intel.com"
+export GIT_PROXY_COMMAND=/usr/local/bin/socks-git
+
+export PATH=$PATH:/home/krom/tools/depot_tools' | tee -a /etc/bash.bashrc
+
+## create socks-git script
+# then set link
+echo '#!/bin/sh
+
+echo $1 | grep "\.intel\.com$" > /dev/null 2>&1
 if [ $? -eq 0 ]; then
     connect $@
 else
     connect -S proxy-socks.jf.intel.com:1080 $@
-fi" >> /usr/local/bin/socks-git
+fi' | tee -a /usr/local/bin/socks-git
 
-# chmod for run.sh
-chmod +x /etc/init.d/run.sh
+
+echo 'PS1=lity@lity-ubuntu-dev
+source /etc/bash.bashrc
+
+## show system env for debug purpose
+export
 
 # config git globals
 git config --global user.email "tianyou.li@gmail.com"
@@ -38,4 +79,12 @@ git config --global push.default matching
 
 # download the goodies
 git clone https://github.com/TianyouLi/goodies.git
+
+# make symbol link
+ln -s ~/goodies/tmux/.tmux.conf ~/.tmux.conf
+ln -s ~/goodies/emacs/.emacs ~/.emacs' | tee /usr/local/bin/lity-ubuntu-dev-env.sh
+
+##swith to user lity
+su -l lity -c "bash /usr/local/bin/lity-ubuntu-dev-env.sh"
+
 
