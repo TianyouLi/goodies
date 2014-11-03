d00d9cae87d3db0e471acc24f70363283625f1ec
Tianyou Li <tianyou.li@gmail.com>
2014-11-03 21:38:02 +0800

    update docker files

--- packer/chromium-dev/scripts/sync.sh
+++ packer/chromium-dev/scripts/sync.sh
@@ -7,7 +7,15 @@ sudo /etc/init.d/privoxy restart
 export http_proxy="http://localhost:8118"
 export https_proxy=$http_proxy
 
-export PATH=/home/lity/depot_tools:$PATH
+echo "
+deb http://archive.ubuntu.com/ubuntu/ precise multiverse
+deb-src http://archive.ubuntu.com/ubuntu/ precise multiverse
+" | tee -a /etc/apt/sources.list
+
+${aptool} update
+${aptool} install lsb-release
+
+export PATH=/home/lity/chromium/depot_tools:$PATH
 ls /home/lity/chromium
 cd /home/lity/chromium/src
 git checkout master
---
+++ packer/teamway/scripts/launch/launch-nginx.sh
@@ -0,0 +1,1 @@
+sudo docker run -d -h lity-nginx --name "lity-nginx" -p 7022:22 lity/nginx:0.1 /bin/bash -e /etc/init.d/run.sh 
