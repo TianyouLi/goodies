8fffeafe56a766f85ed105311ada2d317318339c
Tianyou Li <tianyou.li@gmail.com>
2014-11-11 10:54:14 +0800

    refactor code

--- packer/chromium-dev/scripts/launch.sh
+++ packer/chromium-dev/scripts/launch.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-ID=`sudo docker ps -a | grep lity-chrome-dev | cut -d " " -f 1`
-
-sudo docker stop $ID
-sudo docker rm $ID
-sudo docker run --privileged -d -h lity-chrome-dev --name "lity-chrome-dev" -p 6022:22 tli7-sto.sh.intel.com:8080/chrome-dev:0.3 /bin/bash -e /etc/init.d/run.sh 
+sudo docker run --privileged -d -h lity-chrome-dev --name "lity-chrome-dev" -p 6022:22 -p 5901:5901 -p 6001:6001 tli7-sto.sh.intel.com:8080/chrome-dev:0.3 /bin/bash -e /etc/init.d/run.sh 
