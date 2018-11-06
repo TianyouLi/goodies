35e0574addfa0990f82d406423e177bd85ac8709
Tianyou Li <tianyou.li@intel.com>
2018-11-06 09:14:37 +0800

    git commands

--- git/install.sh
+++ git/install.sh
@@ -25,3 +25,12 @@ BASEDIR=$(dirname $SCRIPT)
 )
 
 
+### git info
+git config --global user.name "Tianyou Li"
+git config --global user.email "tianyou.li@intel.com"
+git config --global core.autocrlf false
+git config --global core.filemode false
+git config --global color.ui true
+
+### configure git cache
+git config --global credential.helper 'cache --timeout=604800'
