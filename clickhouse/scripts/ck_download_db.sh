e043a6dd583b11ea0a327dd4c793475de94fc49c
Tianyou Li <tianyou.li@intel.com>
2024-11-01 10:39:46 +0800

    more ck scripts

---
+++ clickhouse/scripts/ck_download_db.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+CUR_DIR=`pwd`
+SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+usage() {
+    echo "Usage: $0
+    	 	 [-d database dir] 
+                 [-h help]";
+    exit 1;
+}
+
+while getopts ":d:h:" o; do
+    case "${o}" in
+        d)
+            DB_DIR=${OPTARG}
+            ;;
+	h)
+	    usage
+	    ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ -z "${DB_DIR}" ]; then
+    DB_DIR=${CUR_DIR}
+elif [ ! -d "$DB_DIR" ]; then
+    DB_DIR=${CUR_DIR}/${DB_DIR}
+    mkdir -p ${DB_DIR}
+fi
+
+DB_DIR=$( cd "${DB_DIR}" && pwd )
+cd ${DB_DIR}
+
+# download file if necessary, cached in db dir
+if [ ! -f "hits_v1.tsv" ]; then
+        echo "Download test database..."
+        wget https://datasets.clickhouse.com/hits/tsv/hits_v1.tsv.xz
+        wget https://datasets.clickhouse.com/visits/tsv/visits_v1.tsv.xz
+        wget https://datasets.clickhouse.com/hits/tsv/hits_100m_obfuscated_v1.tsv.xz
+
+        xz -v -d hits_v1.tsv.xz
+        xz -v -d visits_v1.tsv.xz
+        xz -v -d hits_100m_obfuscated_v1.tsv.xz
+fi
+
+echo "Database downloaded."
---
+++ clickhouse/scripts/ck_import.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+CUR_DIR=`pwd`
+SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+usage() {
+    echo "Usage: $0
+    	 	 [-d database dir]
+		 [-b build dir]
+		 [-p clickhouse instance port] 
+                 [-h help]";
+    exit 1;
+}
+
+while getopts ":d:b:p:h:" o; do
+    case "${o}" in
+        d)
+            DB_DIR=${OPTARG}
+            ;;
+	p)
+	    PORT=${OPTARG}
+	    ;;
+	b)
+	    BLD_DIR=${OPTARG}
+	    ;;
+	h)
+	    usage
+	    ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+
+if [ -z "${DB_DIR}" ]; then
+    DB_DIR=${CUR_DIR}
+fi
+
+if [ ! -d "${DB_DIR}" ]; then
+    DB_DIR=${CUR_DIR}/${DB_DIR}
+fi
+
+if [ ! -d "${DB_DIR}" ]; then
+    echo "Database dir ${DBDIR} does not exist."
+    exit 1
+fi
+
+if [ -z "${BLD_DIR}" ]; then
+    BLD_DIR=${CUR_DIR}
+fi
+
+DB_DIR=$( cd "${DB_DIR}" && pwd)
+BLD_DIR=$( cd "${BLD_DIR}" && pwd)
+
+CK_BIN=${BLD_DIR}/programs/clickhouse-client
+if [ ! -f ${CK_BIN} ]; then
+    echo "Clickhouse client binary does not exist."
+    exit 1
+fi
+
+if [ -z "${PORT}" ]; then
+    PORT=9000
+fi
+
+if [ ! -f "${DB_DIR}/hits_v1.tsv" ]; then
+    echo "Table data does not exist."
+    exit 1
+fi
+
+
+
+SQL_FILE=$SCRIPT_DIR/sql/test_db_setup.sql
+
+echo "Create tables with command ${CK_BIN} --port $PORT --multiquery < $SQL_FILE"
+${CK_BIN} --port $PORT --multiquery < $SQL_FILE
+
+
+
+echo "Import ${CK_BIN} --port $PORT --max_insert_block_size 100000 --query \"INSERT INTO test.hits FORMAT TSV\" < ${DB_DIR}/hits_v1.tsv"
+${CK_BIN} --port $PORT --max_insert_block_size 100000 --query "INSERT INTO test.hits FORMAT TSV" < ${DB_DIR}/hits_v1.tsv
+
+echo "Import ${CK_BIN} --port $PORT --max_insert_block_size 100000 --query \"INSERT INTO test.visits FORMAT TSV\" < ${DB_DIR}/visits_v1.tsv"
+${CK_BIN} --port $PORT --max_insert_block_size 100000 --query "INSERT INTO test.visits FORMAT TSV" < ${DB_DIR}/visits_v1.tsv
+
+echo "Import ${CK_BIN} --port $PORT --max_insert_block_size 100000 --query \"INSERT INTO hits_100m_single FORMAT TSV\" < ${DB_DIR}/hits_100m_obfuscated_v1.tsv"
+${CK_BIN} --port $PORT --max_insert_block_size 100000 --query "INSERT INTO hits_100m_single FORMAT TSV" < ${DB_DIR}/hits_100m_obfuscated_v1.tsv
+
+
