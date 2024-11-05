a39cfb0072680676d551fa12eaa873942c9dbbbb
Tianyou Li <tianyou.li@intel.com>
2024-11-05 20:10:07 +0800

    improve ck scripts

--- clickhouse/scripts/ck_source.sh
+++ clickhouse/scripts/ck_source.sh
@@ -3,11 +3,37 @@
 CUR_DIR=`pwd`
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-if [ -z "$1" ]; then
+usage() {
+    echo "Usage: $0
+                 [-s source_dir]
+                 [-h help]";
+    exit 1;
+}
+
+while getopts ":s:h:" o; do
+    case "${o}" in
+        b)
+            SRC_DIR=${OPTARG}
+            ;;
+	h)
+	    usage
+	    ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+
+if [ -z "${SRC_DIR}" ]; then
     SRC_DIR=${CUR_DIR}/clickhouse
-else
-    SRC_DIR=${1}
 fi
+
+if [ ! -d "${BLD_DIR}" ]; then
+    SRC_DIR=${CUR_DIR}/${SRC_DIR}
+fi
+
 
 CK_DIR=${SRC_DIR}
 git clone https://github.com/TianyouLi/ClickHouse.git ${CK_DIR}
